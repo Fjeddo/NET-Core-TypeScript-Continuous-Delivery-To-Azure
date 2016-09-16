a4bd878835e4d66ec2aaa4944d717350283210a0
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2016-09-16 10:33:45 +0200

    Empty .NET Core Web Application created and static index.html is served back when requested

---
+++ NetCoreWebApplication/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreWebApplication
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            app.UseStaticFiles();
+        }
+    }
+}
